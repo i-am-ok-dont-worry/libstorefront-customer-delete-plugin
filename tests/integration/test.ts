e4f6afee315eb3e1ffdfebbd1bb7565c6bac9459
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-13 22:44:31 +0100

    Integration test

--- tests/integration/test.ts
+++ tests/integration/test.ts
@@ -1,5 +1,17 @@
 import { LibStorefront } from '@grupakmk/libstorefront';
+import { CustomerDeletePlugin } from '../../src';
+import { CustomerDeleteService } from '../../src/service';
 
 console.warn('Test suite initialized');
 
-const LSF = new LibStorefront();
+const LSF = new LibStorefront({
+    plugins: [
+        CustomerDeletePlugin
+    ]
+}, 'http://localhost:9001');
+
+(async () => {
+    const service = LSF.get(CustomerDeleteService);
+    const res = await LSF.UserService.login('test@grupakmk.pl', 'Testowe2!');
+    const deleteRes = await service.deleteCustomer();
+})();
